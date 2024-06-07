0cf70321122dc50a1742ee52ab04272f187cf7a4
Michael Hauffe <mhauffe26@gmail.com>
2024-06-07 00:42:22 -0500

    Wrote HeatMap code

---
+++ HeatMap.R
@@ -0,0 +1,45 @@
+library(ggcorrplot)
+library(readr)
+library(dplyr)
+
+options(width = 120)
+
+# Read the data
+data <- read_csv("FortWayneRealEstate.csv", col_names = TRUE)
+unfiltereddata <- data
+# Filter and clean data
+data <- data %>%
+  filter(
+    style == 'SINGLE_FAMILY',
+    list_price < 300000,
+    !is.na(sqft),
+    !is.na(latitude),
+    !is.na(longitude),
+    !is.na(stories),
+    !is.na(lot_sqft),
+    !is.na(full_baths),
+    !is.na(half_baths),
+    !is.na(parking_garage),
+    !is.na(sold_price)
+  ) %>%
+  select(
+    sqft, zip_code, beds, full_baths, half_baths, year_built, sold_price,
+    lot_sqft, price_per_sqft, latitude, longitude, stories, hoa_fee, parking_garage
+  )
+
+# Replace hoa_fee null values with 0
+data <- data %>%
+  mutate(hoa_fee = ifelse(is.na(hoa_fee), 0, hoa_fee))
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(data)
+
+# Plot correlation heatmap
+ggcorrplot(correlation_matrix, 
+           hc.order = TRUE, 
+           type = "lower",
+           lab = TRUE,
+           title = "Correlation Heatmap of Variables with Sold Price",
+           ggtheme = ggplot2::theme_minimal(),
+           lab_size = 3)  # Adjust the font size as needed
+
