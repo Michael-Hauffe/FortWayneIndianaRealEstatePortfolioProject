e2251c0f140579c59d6721205ff235f6f6f8ca84
Michael Hauffe <mhauffe26@gmail.com>
2024-06-06 20:14:06 -0500

    started basic analysis

--- Code.R
+++ Code.R
@@ -1,3 +1,14 @@
-#test commit
-1+1
-1+5+7
+library(rvest)
+library(tibble)
+library(httr)
+library(purrr)
+library(dplyr)
+library(readr)
+library(lubridate)
+
+data <- read_csv("FortWayneRealEstate.csv", col_names = TRUE)
+class(data$list_price)
+temp <- data %>% filter(year(last_sold_date) == 2024, !is.na(list_price)) %>% summarize(mean(list_price))
+temp
+
+
